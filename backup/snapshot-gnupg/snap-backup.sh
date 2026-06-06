59958268ed84bf97aa7a9997d7b99b55b5ba42af
bousahla-mounir <bousahlamounir@gmail.com>
2026-06-06 12:12:16 +0100

    snapshot backup

---
+++ backup/snapshot-gnupg/snap-backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+### Create: nano /usr/local/bin/snap-backup.sh 
+
+VG_NAME="vg_system"
+LV_NAME="lv-home"
+SNAP_NAME="snap-daily"
+SNAP_SIZE="5G"
+
+# 1. Clean up any old crashed snapshots
+if lvs $VG_NAME/$SNAP_NAME > /dev/null 2>&1; then
+    lvremove -f $VG_NAME/$SNAP_NAME
+fi
+
+# 2. Create the snapshot
+lvcreate --size $SNAP_SIZE --snapshot --name $SNAP_NAME /dev/$VG_NAME/$LV_NAME
+
+# 3. Use the snapshot (e.g., rsync it to an external disk)
+mkdir -p /mnt/snap_mount
+mount /dev/$VG_NAME/$SNAP_NAME /mnt/snap_mount
+tar czf - /mnt/snap_mount | sudo gpg --batch --yes --encrypt --recipient nginxwebserver@gmail.com --trust-model always --output /free/backup/backup.tar.gz.gpg
+umount /mnt/snap_mount
+# 2. Rsync the ENCRYPTED file only
+rsync -az /free/backup/backup.tar.gz.gpg hilal@192.168.100.214:~/backup_folder
+# 4. Remove the snapshot (Keep the 'write' performance high)
+#lvremove -f $VG_NAME/$SNAP_NAME
+
+
